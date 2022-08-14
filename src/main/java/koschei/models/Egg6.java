e1dff46a4d54d0655c93d7a0ccd1e107a635046c
Евгения <jane_79@list.ru>
2022-08-14 17:21:35 +0300

    1st commit

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("eggBean")
 public class Egg6 {
+
+    private Needle7 needle;
+
+    @Autowired                                  // я добавила
+    public void setNeedle(Needle7 needle) {
+        this.needle = needle;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }
--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("rabbitBean")               // bean id - добавила я
 public class Rabbit4 {
+
+    private final Duck5 duck;                    // это я добавила
+
+    @Autowired
+    public Rabbit4(Duck5 duck) {                  // добавила конструктор
+        this.duck = duck;
+    }
 
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + duck.toString();
     }
 }
